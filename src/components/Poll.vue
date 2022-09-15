d9c51c172872a7a0e8e13f9ff5430696239a2ddf
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-09-15 15:46:20 +0800

    added: poll options

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,10 +1,33 @@
 <template>
-  <h1>Poll</h1>
+  <div class="row">
+    <div class="col">
+      <h1>Poll Title</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur iste,
+        autem dolor nesciunt necessitatibus quibusdam similique error totam.
+        Expedita, incidunt? Ipsum architecto debitis corporis pariatur nesciunt
+        aperiam commodi delectus officiis!
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="list-group">
+        <PollOption />
+        <PollOption />
+        <PollOption />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PollOption from "./PollOption.vue";
 export default {
   name: "Poll",
+  components: {
+    PollOption,
+  },
 };
 </script>
 
---
+++ src/components/PollOption.vue
@@ -0,0 +1,25 @@
+<template>
+  <a href="#" class="list-group-item list-group-item-action">
+    <h6>Option Title</h6>
+    <div class="progress" style="height: 30px">
+      <div
+        class="progress-bar"
+        role="progressbar"
+        style="width: 25%"
+        aria-valuenow="25"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        25%
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "PollOption",
+};
+</script>
+
+<style></style>
